70c02a3ea9b3e78969030c1e227f881b48957ede
alab1020 <hakimalmestrehi@gmail.com>
2025-11-05 14:25:24 +0000

    test successfuly passed

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -1,16 +1,12 @@
-"""
-Client and server using classes
-"""
-
 import logging
 import socket
 
 import const_cs
-#from context import lab_logging
+from context import lab_logging
 
-#lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
+lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
 
-# pylint: disable=logging-not-lazy, line-too-long
+#pylint: disable=logging-not-lazy, line-too-long
 
 class Server:
     """ The server """
@@ -24,15 +20,65 @@ class Server:
         self.sock.settimeout(3)  # time out in order not to block forever
         self._logger.info("Server bound to socket " + str(self.sock))
 
-        # Telefonbuchs
-        
-        self.telefonbuch = {}
-        for i in range(1,300):
-            name =f"user{i}"
-            nummer = 100000 + i * 35 
-            self.telefonbuch[name] = nummer
+        self.telefonbuch = {
+            "Alpha": "0176-12345678",
+            "Beta": "0176-23456789", 
+            "Gamma": "0176-123123123",
+            "Hakim": 13245,
+            "Florian": 13245,
+            "Yusuf": 13245, 
+            "Korn": 13245,
+            "Weber": 54321,
+            "Hofmann": 15643,
+            "Mueller": 49122,
+            "Schmidt": 49233,
+            "Schneider": 49344,
+            "Fischer": 49455,
+            "Meyer": 49566,
+            "Wagner": 49677,
+            "Becker": 49788,
+            "Schulz": 49899,
+            "Hoffmann": 49910,
+            "Koch": 50011,
+            "Bauer": 50022,
+            "Richter": 50033,
+            "Klein": 50044,
+            "Wolf": 50055,
+            "Neumann": 50066,
+            "Schwarz": 50077,
+            "Zimmermann": 50088,
+            "Braun": 50099,
+            "Krueger": 50110,
+            "Hartmann": 50132,
+            "Lange": 50143,
+            "Schmitt": 50154,
+            "Werner": 50165,
+            "Schmitz": 50176,
+            "Krause": 50187,
+            "Meier": 50198,
+            "Lehmann": 50209,
+            "Schmid": 50220,
+            "Schaefer": 50231,
+            "Maier": 50242,
+            "Keller": 50253,
+            "Huber": 50264,
+            "Mayer": 50275,
+            "Kaiser": 50286,
+            "Vogel": 50297,
+            "Fuchs": 50308,
+            "Lang": 50319,
+            "Weiss": 50330,
+            "Peters": 50341,
+            "Scholz": 50352,
+            "Jung": 50363,
+            "Moeller": 50374,
+            "Hahn": 50385,
+            "Koenig": 50396,
+            "Walter": 50407,
+            "Kaiser": 50418
+        }
 
-        
+
 
     def handle_request(self, request):
         if request.startswith("GETALL"):
@@ -73,7 +119,7 @@ class Client:
         self.sock.connect((const_cs.HOST, const_cs.PORT))
         self.logger.info("Client connected to socket " + str(self.sock))
 
-    def send_request(self, request):
+    def call(self, request):
         """ Call server """
         self.sock.send(request.encode('ascii'))  # send encoded string as data
         data = self.sock.recv(1024)  # receive the response
@@ -83,16 +129,13 @@ class Client:
         return response
 
     def GET(self, name):
-        response = self.send_request(f"GET {name}")
+        response = self.call(f"GET {name}")
         print(response)
     
     def GETALL(self):
-        response = self.send_request("GETALL")
+        response = self.call("GETALL")
         print(response)
 
     def close(self):
         """ Close socket """
         self.sock.close()
-"""
-Client and server using classes
-"""
